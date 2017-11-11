be73a3cf3ef993944bc7bce662e46a498a914978
Sandip Singha <singha.sandip@gmail.com>
2017-11-11 17:49:12 +0530

    Data Science

---
+++ Data Scientist Courses/financial aid.sql
@@ -0,0 +1,33 @@
+Hi,
+
+I am Sandip Singha , an IT resource having 15 years of experience in this filed.  My  IT experiences are  in different technologies like oracle, teradata, cognos, erwin data modeling tool etc. I am working with IBM India.
+
+My mother, wife , daughter are dependent on me. My daughter is in class 7 and there is a huge educational expense too. My mother is 66 years old. There is a regular medical expenses too.
+
+I have a home loan for tanure of 10 years and I have to pay INR 12000/- Monthly . Hence taking another loan is not possible for me. At the same time I am very mush excited doing the Data Science Specialization course from your university.
+
+There are so many suggestions, so much material, so many options, it becomes difficult to know what to learn first. There’s a mountain of content, and it’s difficult to know where to find the gold nuggets, the things to learn that will bring you the high return on time investment.
+
+And that’s the crux of the problem. The fact is, time is limited. Learning a new programming language is a large investment of time, so I need to be strategic about which one I select.
+
+To be clear, some languages will yield a very high return on my investment (your investment of both time and money). Other languages are purely auxiliary tools that I might use only a few times per year. 
+
+I I want to learn data science.  Hence it would be great if I get a financial aid.
+
+
+My  IT experiences are  in different technologies like oracle, teradata, cognos, erwin data modeling tool etc. I am working with IBM India.
+
+R programming is one of the cutting age technology  now a days. I think if I learn it it would give me enough boosting in my career. I am very mush excited doing the Data Science Specialization course from your university.
+Hence it would be great if I get a financial aid.
+
+There are so many suggestions, so much material, so many options, it becomes difficult to know what to learn first. There’s a mountain of content, and it’s difficult to know where to find the gold nuggets, the things to learn that will bring you the high return on time investment.
+
+And that’s the crux of the problem. The fact is, time is limited. Learning a new programming language is a large investment of time, so I need to be strategic about which one I select.
+
+To be clear, some languages will yield a very high return on my investment (your investment of both time and money). Other languages are purely auxiliary tools that I might use only a few times per year. 
+
+I want to learn data science.  Hence it would be great if I get a financial aid.
+
+My mother, wife , daughter are dependent on me. I have a home loan too and my daughter is in class 7 and henec there is a huge educational expense too. it would be great if I get a financial aid.
+
+
